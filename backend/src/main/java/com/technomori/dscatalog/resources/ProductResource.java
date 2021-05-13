56fb0f96dbb54f0baf788d88e92be51adf9b2fbb
Silvio Mori Neto <silviomori@gmail.com>
2021-05-13 20:41:24 -0300

    Create endpoints to Create, Update, Delete product

--- backend/src/main/java/com/technomori/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/technomori/dscatalog/resources/ProductResource.java
@@ -1,15 +1,22 @@
 package com.technomori.dscatalog.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.technomori.dscatalog.dto.ProductDTO;
 import com.technomori.dscatalog.services.ProductService;
@@ -38,24 +45,24 @@ public class ProductResource {
 		return ResponseEntity.ok().body(productService.findById(id));
 	}
 
-//	@PostMapping
-//	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO productDTO) {
-//		productDTO = productService.insert(productDTO);
-//		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-//					.path("/{id}").buildAndExpand(productDTO.getId()).toUri();
-//		return ResponseEntity.created(uri).body(productDTO);
-//	}
-//
-//	@PutMapping(value = "/{id}")
-//	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
-//		productDTO = productService.update(id, productDTO);
-//		return ResponseEntity.ok().body(productDTO);
-//	}
-//
-//	@DeleteMapping(value = "/{id}")
-//	public ResponseEntity<Void> update(@PathVariable Long id) {
-//		productService.delete(id);
-//		return ResponseEntity.noContent().build();
-//	}
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO productDTO) {
+		productDTO = productService.insert(productDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{id}").buildAndExpand(productDTO.getId()).toUri();
+		return ResponseEntity.created(uri).body(productDTO);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
+		productDTO = productService.update(id, productDTO);
+		return ResponseEntity.ok().body(productDTO);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@PathVariable Long id) {
+		productService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 
 }
--- backend/src/main/java/com/technomori/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/technomori/dscatalog/services/ProductService.java
@@ -1,6 +1,10 @@
 package com.technomori.dscatalog.services;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -8,7 +12,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.technomori.dscatalog.dto.ProductDTO;
 import com.technomori.dscatalog.entities.Product;
+import com.technomori.dscatalog.exceptions.DatabaseException;
 import com.technomori.dscatalog.exceptions.ResourceNotFoundException;
+import com.technomori.dscatalog.repositories.CategoryRepository;
 import com.technomori.dscatalog.repositories.ProductRepository;
 
 @Service
@@ -16,6 +22,8 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository productRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
@@ -33,37 +41,50 @@ public class ProductService {
 		return new ProductDTO(product, product.getCategories());
 	}
 
-//	@Transactional
-//	public ProductDTO insert(ProductDTO productDTO) {
-//		Product productEntity = new Product();
-//		productEntity.setName(productDTO.getName());
-//		productEntity = productRepository.save(productEntity);
-//		return new ProductDTO(productEntity);
-//	}
-//
-//	@Transactional
-//	public ProductDTO update(Long id, ProductDTO productDTO) {
-//		try {
-//			Product productEntity = productRepository.getOne(id);
-//			productEntity.setName(productDTO.getName());
-//			productEntity = productRepository.save(productEntity);
-//			return new ProductDTO(productEntity);
-//		} catch (EntityNotFoundException e) {
-//			throw new ResourceNotFoundException(
-//					String.format("Product ID %d not found", id));
-//		}
-//	}
-//
-//	public void delete(Long id) {
-//		try {
-//			productRepository.deleteById(id);
-//		} catch (EmptyResultDataAccessException e) {
-//			throw new ResourceNotFoundException(
-//					String.format("Product ID %d not found", id));
-//		} catch (DataIntegrityViolationException e) {
-//			throw new DatabaseException(
-//					String.format("Integrity violation: Product ID %d", id));
-//		}
-//	}
+	@Transactional
+	public ProductDTO insert(ProductDTO productDTO) {
+		Product productEntity = new Product();
+		copyDataFromDtoToEntity(productDTO, productEntity);
+		productEntity = productRepository.save(productEntity);
+		return new ProductDTO(productEntity);
+	}
+
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO productDTO) {
+		try {
+			Product productEntity = productRepository.getOne(id);
+			copyDataFromDtoToEntity(productDTO, productEntity);
+			productEntity = productRepository.save(productEntity);
+			return new ProductDTO(productEntity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(
+					String.format("Product ID %d not found", id));
+		}
+	}
+
+	private void copyDataFromDtoToEntity(ProductDTO productDTO, Product productEntity) {
+		productEntity.setDate(productDTO.getDate());
+		productEntity.setDescription(productDTO.getDescription());
+		productEntity.setImgUrl(productDTO.getImgUrl());
+		productEntity.setName(productDTO.getName());
+		productEntity.setPrice(productDTO.getPrice());
+
+		productEntity.getCategories().clear();
+		productDTO.getCategories().forEach(catDTO -> {
+			productEntity.getCategories().add(categoryRepository.getOne(catDTO.getId()));
+		});
+	}
+
+	public void delete(Long id) {
+		try {
+			productRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(
+					String.format("Product ID %d not found", id));
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(
+					String.format("Integrity violation: Product ID %d", id));
+		}
+	}
 
 }
