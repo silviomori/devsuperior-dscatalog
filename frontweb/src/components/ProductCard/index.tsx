ef45d2797b00dd96e7893dc834b5edc7ec23441a
Silvio Mori Neto <silviomori@gmail.com>
2021-09-02 14:48:14 -0300

    Use props to set ProductCard, ProductPrice data

--- frontweb/src/components/ProductCard/index.tsx
+++ frontweb/src/components/ProductCard/index.tsx
@@ -1,16 +1,20 @@
 import './styles.css';
-import ProductPng from 'assets/images/product.png';
 import ProductPrice from 'components/ProductPrice';
+import { Product } from 'types/product';
 
-const ProductCard = () => {
+type Props = {
+  product: Product;
+};
+
+const ProductCard = ({ product }: Props) => {
   return (
     <div className="base-card product-card">
       <div className="card-top-container">
-        <img src={ProductPng} alt="Product Name" />
+        <img src={product.imgUrl} alt={product.name} />
       </div>
       <div className="card-bottom-container">
-        <h6>Desktop Computer - Intel Core i7</h6>
-        <ProductPrice />
+        <h6>{product.name}</h6>
+        <ProductPrice price={product.price} />
       </div>
     </div>
   );
