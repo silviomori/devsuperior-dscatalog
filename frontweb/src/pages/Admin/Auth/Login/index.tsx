8c9a386591f177334d9b089e88cf1a591292fdee
Silvio Mori Neto <silviomori@gmail.com>
2021-09-06 07:54:02 -0300

    Create the skeleton of the sign-in card

---
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+import ButtonIcon from 'components/ButtonIcon';
+
+import './styles.css';
+
+const Login = () => {
+  return (
+    <div className="base-card login-card">
+      <h1>Sign-In</h1>
+      <form>
+        <div className="mb-4">
+          <input
+            type="text"
+            className="form-control base-input"
+            placeholder="Email"
+            name="username"
+          />
+        </div>
+        <div className="mb-2">
+          <input
+            type="password"
+            className="form-control base-input "
+            placeholder="Password"
+            name="password"
+          />
+        </div>
+        <Link to="/admin/auth/recover" className="login-link-recover">
+          Esqueci a senha
+        </Link>
+        <div className="login-submit">
+          <ButtonIcon text="Sign-in" />
+        </div>
+        <div className="signup-container">
+          <span className="not-registered">New to DS Catalog?</span>
+          <Link to="/admin/auth/register" className="login-link-register">
+            Create account
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- frontweb/src/pages/Admin/Auth/index.tsx
+++ frontweb/src/pages/Admin/Auth/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import { ReactComponent as AuthImage } from 'assets/images/auth-image.svg';
 import { Route, Switch } from 'react-router-dom';
+import Login from './Login';
 
 const Auth = () => {
   return (
@@ -13,7 +14,7 @@ const Auth = () => {
       <div className="base-card auth-form-container">
         <Switch>
           <Route path="/admin/auth/login">
-            <h1>Card Login</h1>
+            <Login />
           </Route>
           <Route path="/admin/auth/signup">
             <h1>Card Sign Up</h1>
