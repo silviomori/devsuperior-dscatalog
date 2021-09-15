24e48897cefb5fde94d88cffd80a6a2e4f7a9220
Silvio Mori Neto <silviomori@gmail.com>
2021-09-15 17:28:35 -0300

    Include buttons in the product crud card

--- frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -10,12 +10,18 @@ const ProductCrudCard = () => {
         <img src={ProductPng}></img>
       </div>
       <div className="product-crud-card-details-container">
-        <h6>Desktop Computer - Intel Core i7</h6>
-        <ProductPrice price={2779.0} />
-        <div className="product-crud-card-categories-container">
-          <CategoryBadge name="Cat 1" />
-          <CategoryBadge name="Cat 2" />
-        </div>
+          <h6>Desktop Computer - Intel Core i7</h6>
+          <ProductPrice price={2779.0} />
+          <div className="product-crud-card-categories-container">
+            <CategoryBadge name="Electronics" />
+            <CategoryBadge name="Computer" />
+            <CategoryBadge name="Deluxe" />
+            <CategoryBadge name="New" />
+          </div>
+      </div>
+      <div className="product-crud-card-buttons-container">
+        <button className="btn btn-outline-danger product-crud-card-button">Delete</button>
+        <button className="btn btn-outline-secondary product-crud-card-button">Edit</button>
       </div>
     </div>
   );
