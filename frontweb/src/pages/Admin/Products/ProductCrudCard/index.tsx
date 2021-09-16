1d092254ec5f62f476e1aa624389369001712a67
Silvio Mori Neto <silviomori@gmail.com>
2021-09-16 20:13:39 -0300

    Include feature to delete a product

--- frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -3,16 +3,36 @@ import ProductPrice from 'components/ProductPrice';
 import CategoryBadge from '../CategoryBadge';
 import { Product } from 'types/product';
 import { Link } from 'react-router-dom';
+import { AxiosRequestConfig } from 'axios';
+import { requestBackend } from 'util/requests';
 
 type Props = {
   product: Product;
 };
 
 const ProductCrudCard = ({ product }: Props) => {
+  const handleDelete = (productId: number) => {
+    if (!window.confirm("Are you sure you want to delete this product?")) {
+      return;
+    }
+    const config: AxiosRequestConfig = {
+      method: 'DELETE',
+      url: `/products/${productId}`,
+      withCredentials: true,
+    };
+    requestBackend(config).then(() => {
+      console.log('Product deleted.');
+    });
+  };
+
   return (
     <div className="base-card product-crud-card-container">
       <div className="product-crud-card-image-container">
-        <img className="img-fluid" src={product.imgUrl} alt={product.name}></img>
+        <img
+          className="img-fluid"
+          src={product.imgUrl}
+          alt={product.name}
+        ></img>
       </div>
       <div className="product-crud-card-details-container">
         <h6>{product.name}</h6>
@@ -24,7 +44,10 @@ const ProductCrudCard = ({ product }: Props) => {
             ))}
         </div>
       </div>
-      <div className="product-crud-card-buttons-container">
+      <div
+        className="product-crud-card-buttons-container"
+        onClick={() => handleDelete(product.id)}
+      >
         <button className="btn btn-outline-danger product-crud-card-button">
           Delete
         </button>
