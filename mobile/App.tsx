a667fbd07f6026ae8a6e6d8f35d942859305cb93
Silvio Mori <silviomori@gmail.com>
2022-08-15 17:43:07 -0700

    Create project for the mobile app

---
+++ mobile/App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+};
+
+export default App;
